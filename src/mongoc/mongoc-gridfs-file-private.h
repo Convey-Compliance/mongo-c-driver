7a376044572ba6d85f9d8c9528330d208048347b
Jose Sebastian Battig <jsbattig@convey.com>
2015-07-27 00:00:44 -0500

    B-0 added pos_after_read_write to gridfs file structure

--- src/mongoc/mongoc-gridfs-file-private.h
+++ src/mongoc/mongoc-gridfs-file-private.h
@@ -59,6 +59,7 @@ struct _mongoc_gridfs_file_t
    const char                                 *bson_content_type;
    bson_t                                      bson_aliases;
    bson_t                                      bson_metadata;
+   bool                                        pos_after_read_write;
    const mongoc_gridfs_file_chunk_callbacks_t *chunk_callbacks;
    void                                       *chunk_callbacks_custom_data;
 };
